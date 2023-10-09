6b9d35c1c58185b8d122dcc4df3abae1e64a9464
Jason Kirby <jason@jasonkirby.com>
2023-10-08 20:59:39 -0700

    Telescope & Treesitter

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,7 @@
+
+require("core.keymaps")
+require("core.plugins")
+require("core.plugin_config")
+require("core.plugin_config.nvim-tree")
+require("core.plugin_config.treesitter")
+require("core.plugin_config.telescope")
---
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -0,0 +1,19 @@
+
+
+
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+ 
+
+vim.o.scrolloff = 15
+vim.opt.relativenumber = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth=2
+vim.opt.shiftround=true
+vim.opt.number = true
+vim.opt.expandtab=true
+vim.opt.smartindent=true
+vim.opt.autoread=true
+
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
