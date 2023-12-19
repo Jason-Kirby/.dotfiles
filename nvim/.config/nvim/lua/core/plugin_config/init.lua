36cda67db7e68d1c7c606c89504efe0c9784633e
Jason-Kirby <jason@jasonkirby.com>
2023-12-18 20:06:56 -0800

    updates to nvim telescope

--- nvim/.config/nvim/lua/core/plugin_config/init.lua
+++ nvim/.config/nvim/lua/core/plugin_config/init.lua
@@ -8,3 +8,10 @@ require("core.plugin_config.eldar")
 require("core.plugin_config.moonbow")
 
 require("core.plugin_config.ayu_dark")
+
+
+require("core.plugin_config.treesitter")
+require("core.plugin_config.telescope")
+require("core.plugin_config.lsp_config")
+require("core.plugin_config.gitsigns")
+require("core.plugin_config.completions")
